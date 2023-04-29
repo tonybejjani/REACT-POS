0bfd5b1ab3be928c1de388f7e40bfc77f1b0e477
Tony <bejjani.tony@gmail.com>
2023-04-29 16:05:10 +1000

    add reducer to Context provider to accept new menu items added from the MenuPanel

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,18 +1,51 @@
 /** @format */
 import { useReducer } from 'react';
+
 import CartContext from './cart-context';
 
-const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
 
-  const removeItemFromCartHandler = (id) => {};
+const CART_ACTIONS = {
+  ADD_ITEM: 'add-item',
+  REMOVE_ITEM: 'remove-item',
+};
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case CART_ACTIONS.ADD_ITEM:
+      return {
+        items: [...state.items, action.item],
+        totalAmount: state.totalAmount + action.item.price,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const CartProvider = (props) => {
+  const [cartState, dispatchCartActions] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+  const addItemToCartHandler = (item) => {
+    dispatchCartActions({ type: CART_ACTIONS.ADD_ITEM, item: item });
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartActions({ type: CART_ACTIONS.REMOVE_ITEM, id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
+
+  console.log(cartState);
   return (
     <CartContext.Provider value={cartContext}>
       {props.children}
